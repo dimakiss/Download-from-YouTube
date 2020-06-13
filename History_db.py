c44b916bbc2ce4392f0e763d52621ee6531d6e1d
dimakiss <40802862+dimakiss@users.noreply.github.com>
2020-06-14 00:39:42 +0300

    Create History_db.py

---
+++ History_db.py
@@ -0,0 +1,20 @@
+import os,shutil,sqlite3
+
+
+class History_db():
+    def __init__(self):
+        self.data=self.get_history_data_base()
+
+    def get_history_data_base(self):
+        path =os.path.expanduser('~' ) +"\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\"
+        shutil.copyfile(path +"History",
+                        path +"History1")
+
+        # Read sqlite query results into a pandas DataFrame
+        con = sqlite3.connect(path +"History1")
+        c = con.cursor()
+        c.execute("SELECT * FROM urls ORDER BY last_visit_time")
+        data = c.fetchall()
+        con.close()
+        os.remove(path +"History1")
+        return data
