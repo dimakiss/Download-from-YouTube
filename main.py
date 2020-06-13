a44e8c963b3209672d30b66f1864a1a34f54f281
dimakiss <40802862+dimakiss@users.noreply.github.com>
2020-06-13 19:21:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,62 @@
 from __future__ import unicode_literals
 from bs4 import BeautifulSoup
 from datetime import datetime
-from shutil import copyfile
-import os,calendar,sqlite3,timeit,requests,youtube_dl,glob,sys,multiprocessing
+import os,calendar,sqlite3,timeit,requests,youtube_dl,glob,sys,multiprocessing,shutil,time
 from itertools import product
 from moviepy.editor import *
 start = timeit.default_timer()
 
 number_of_processors=8
-
+def move():
+    counter=0
+    #for file in glob.glob("*.mp3"):
+    #    try:
+    #        shutil.move(file, os.path.join(os.getcwd() + "\\downloads_\\",
+    #                                       (str(file))[:-16] + "." + (str(file)).split('.')[-1]))
+    #        counter+=1
+    #    except:
+    #        os.remove(file)
+    #return counter
+    return glob.glob("*.mp3").__len__()
+text=[]
 def dowload(url):
+
     ydl_opts = {}
-    try:
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-        for file in glob.glob("*.mp4"):
-            if url[-10:-1] in file:
-                video = VideoFileClip(os.path.join(file))
-                try:
-                    video.audio.write_audiofile(
-                        os.path.join(os.getcwd() + "\\downloads_\\", (str(file))[:-16] + ".mp3"))
-                    video.close()
-                    os.remove(os.getcwd() + "\\" + file)
-                    return 1
-                except:
-                    video.close()
-                    os.remove(os.getcwd() + "\\" + file)
-                    return 0
-    except:
-        pass
+    for i in range(20):
+        try:
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }]}
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+            return 1
+        except:
+            if i>10:
+                time.sleep(1)
+            pass
+    text.append(url)
+#def donwload(url)
     
 def is_music(url):
+    url="https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet&id="+url.split('watch?v=')[1]+"&key="
+    key="AIzaSyCIFQIwDZ8CQMYiXn6BYKbUXgCvHnKAw9g"
+    url+=key
     result = requests.get(url).text
-    soup = BeautifulSoup(result, 'lxml')
-    return str(soup).split("UC-9-kyTW8ZkZNDHQJ6FgpwQ").__len__()!=1
+    for i in result.split('\n'):
+        if "categoryId" in i:
+            return "10" in i
+    return False
+
+#"https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet&id=8dl5ZkSnQ6c&key=AIzaSyCIFQIwDZ8CQMYiXn6BYKbUXgCvHnKAw9g"
+
 
 def get_urls(months,number_of_views):
     path=os.path.expanduser('~')+"\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\"
-    copyfile(path+"History",
+    shutil.copyfile(path+"History",
              path+"History1")
 
     # Read sqlite query results into a pandas DataFrame
@@ -52,7 +71,7 @@ def get_urls(months,number_of_views):
     url = []
     count = []
     for row in data:
-        if "youtube.com/watch?v=" in row[1] and row[5] != 0:
+        if "https://www.youtube.com/watch?v=" == row[1][:32] and row[5] != 0:
             time = int(row[5]) // 1000000 - 11644473600
             if int(calendar.timegm(datetime.utcnow().utctimetuple())) - months * 30 * 24 * 60 * 60 <= time:
                 r = str(row[2]).split(" - YouTube")[0]
@@ -103,8 +122,8 @@ def save_data_to_text(data):
 if __name__ == '__main__':
     number_of_files=0
     number_of_processors = 8
-    months=1
-    watch_times=5
+    months=4
+    watch_times=10
     if sys.argv.__len__() == 4:
         months=float(sys.argv[1])
         watch_times=int(sys.argv[2])
@@ -112,12 +131,13 @@ if __name__ == '__main__':
     if os.path.isdir(os.getcwd() + '\\downloads_') == False:
         os.mkdir(os.getcwd()+"\\downloads_")
     urls=get_urls(months,watch_times)
-    results=multi_processing(dowload,urls)
-    for i in results:
-        if i==1:
-            number_of_files+=1
+    os.chdir(os.getcwd() + "\\downloads_")
+    number_of_processors=5
+    multi_processing(dowload, urls)
+    number_of_files=move()
+
     clear = lambda: os.system('cls')
     clear()
     stop = timeit.default_timer()
-    print("Files downloaded: "+str(number_of_files)+"\t Took: ",int(stop - start),"s")
+    print("Files downloaded: "+str(number_of_files)+"\t Took: ",int(stop - start),"s",urls.__len__(),text)
 
